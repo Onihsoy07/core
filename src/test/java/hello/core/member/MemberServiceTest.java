17fd96db8dbd1b0185eb18a04ec6fd5c144602ac
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-05-28 19:50:46 +0900

    Member Join 구현 및 테스트

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,25 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        //given
+        Member member1 = new Member(1L, "member1", Grade.VIP);
+
+        //when
+        memberService.join(member1);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member1).isEqualTo(findMember);
+    }
+
+}
