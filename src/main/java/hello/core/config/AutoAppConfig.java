a5a84b0a30f5de2bf9a61462cacfc3ead87fa236
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-06-01 00:01:19 +0900

    빈 이름 충돌 시

--- src/main/java/hello/core/config/AutoAppConfig.java
+++ src/main/java/hello/core/config/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core.config;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +14,10 @@ import org.springframework.stereotype.Component;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    @Bean("memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
 }
