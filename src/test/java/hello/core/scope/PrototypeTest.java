72a7df6fd95447587f03da65f9bd01062a83e58a
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-06-04 16:47:33 +0900

    Singleton에서 Prototype DI로 사용시 문제됨
    다음에 수정

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,44 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("prototypeBean1 = " + prototypeBean1);
+        System.out.println("prototypeBean2 = " + prototypeBean2);
+        assertThat(prototypeBean1).isNotSameAs(prototypeBean2);
+
+        //prototype은 빈 생성 후 스프링에서 관리를 안하므로 @PreDestroy 동작 안함
+        //따로 destroy()를 해줘야함 -> 머지 버전이 달라서 그런가 없네 close() 해주면 종료됨
+        prototypeBean1.close();
+        prototypeBean2.close();
+
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void close() {
+            System.out.println("SingletonBean.close");
+        }
+    }
+}
---
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,95 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonWithPrototypeTest1 {
+
+    //prototype의 빈이므로 addCount() 호출을 해도 각각 count가 올라감
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+
+    }
+
+    //singleton 내의 주입되는 prototype은 생성자 주입으로 초기에만 호출하므로 사용시 prototype 새로 생성 안됨
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class, SingletonBean.class);
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+
+        int singletonBean1Count = singletonBean1.logic();
+        assertThat(singletonBean1Count).isEqualTo(1);
+
+        int singletonBean2Count = singletonBean2.logic();
+        assertThat(singletonBean2Count).isEqualTo(2);
+
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        public SingletonBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init : " + this);
+        }
+
+        @PreDestroy
+        public void close() {
+            System.out.println("SingletonBean.close : " + this);
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public int getCount() {
+            return count;
+        }
+
+        public void addCount() {
+            count++;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init : " + this);
+        }
+
+        @PreDestroy
+        public void close() {
+            System.out.println("PrototypeBean.close : " + this);
+        }
+    }
+
+}
